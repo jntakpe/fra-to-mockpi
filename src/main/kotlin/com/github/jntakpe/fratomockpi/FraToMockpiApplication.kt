29b6041f488dc92f230a4954c83bcfa177c34dd1
jntakpe <j.ntakpe@gmail.com>
2017-05-09 21:03:04 +0200

    Default configuration

---
+++ src/main/kotlin/com/github/jntakpe/fratomockpi/FraToMockpiApplication.kt
@@ -0,0 +1,11 @@
+package com.github.jntakpe.fratomockpi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FraToMockpiApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(FraToMockpiApplication::class.java, *args)
+}
